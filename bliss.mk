9342e52cf93546aff660fd975cb9e69641c363d0
Apavayan <info@apavayan.com>
2015-04-16 13:36:12 +0530

    Took step back

--- bliss.mk
+++ bliss.mk
@@ -21,3 +21,6 @@ $(call inherit-product, device/samsung/ms013g/full_ms013g.mk)
 # Release name
 PRODUCT_RELEASE_NAME := SM-G7102
 PRODUCT_NAME := bliss_ms013g
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ms013gxx TARGET_DEVICE=ms013g BUILD_FINGERPRINT="samsung/ms013gxx/ms013g:4.4.2/KOT49H/G7102XXUBNK1:user/release-keys" PRIVATE_BUILD_DESC="ms013gxx-user 4.4.2 KOT49H G7102XXUBNK1 release-keys"
+
