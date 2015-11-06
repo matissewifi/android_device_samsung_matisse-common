de7fcb2aa1037aae5727a1eb6e5dda446ecd0499
Apavayan Sinha <info@apavayan.com>
2015-11-06 21:09:41 +0530

    ms013g: add Dual sim audio Hal
    Signed-off-by: Apavayan Sinha <info@apavayan.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,16 @@
 # Inherit from msm8226-common
 -include device/samsung/msm8226-common/BoardConfigCommon.mk
 
-TARGET_OTA_ASSERT_DEVICE := ms013g,ms01lte
-
 LOCAL_PATH := device/samsung/ms013g
+
+# Audio
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
+
+# Radio
+SIM_COUNT := 2
+TARGET_GLOBAL_CFLAGS += -DANDROID_MULTI_SIM
+TARGET_GLOBAL_CPPFLAGS += -DANDROID_MULTI_SIM
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
@@ -29,9 +36,8 @@ BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardwa
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
-TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
-TARGET_KERNEL_CONFIG := msm8226-sec_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_ms013g_eur_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/ms013g
+TARGET_KERNEL_CONFIG := cyanogenmod_ms013g_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 
 # Partitions
