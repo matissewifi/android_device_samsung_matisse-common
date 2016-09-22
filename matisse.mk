d864f6b63dc2f1e824e70a6ddbde5526a16a6e12
sub77 <sub77@ymail.comgit config --global user.email sub77@ymail.comgit config --global user.email sub77@ymail.com>
2016-09-22 12:02:58 +0000

    matisse-common: Move gps.conf to msm8226-common

--- matisse.mk
+++ matisse.mk
@@ -48,10 +48,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
-#GPS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
