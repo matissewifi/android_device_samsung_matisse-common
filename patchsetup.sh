94496f1488f6b699c1ec80be3207c6513439f16a
sub77 <sub77@ymail.com>
2016-09-24 23:59:00 +0000

    matisse-common: move patchbase to msm8226-common

--- patchsetup.sh
+++ patchsetup.sh
@@ -1,1 +1,2 @@
 bash `test -d device && find -L device/samsung/matisse-common/patch -maxdepth 4 -name 'patch.sh' 2> /dev/null`
+bash `test -d device && find -L device/samsung/msm8226-common/patch -maxdepth 4 -name 'patch.sh' 2> /dev/null`
