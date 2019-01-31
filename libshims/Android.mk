d68a19dc5534836ec195f75df2faaffb044e4b47
matteo0026 <marmotta200026@gmail.com>
2019-01-31 08:19:36 +0100

    matisse-common: add libmmcamera_sr352_shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -15,3 +15,10 @@ LOCAL_MODULE := libshim_wvm
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libmmcamera_sr352_shim.cpp
+LOCAL_MODULE := libmmcamera_sr352_shim
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
