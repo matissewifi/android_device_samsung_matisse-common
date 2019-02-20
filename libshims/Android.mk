d64247321307cbc47a5b26f804e4a93c21020f1d
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-02-20 10:25:08 -0500

    libmmcamera_sr130pc20 -> libmmcamera_sr130pc20_shim
    libmmcamera_sr130pc20 -> libmmcamera_sr130pc20_shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -17,8 +17,8 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libmmcamera_sr130pc20.cpp
-LOCAL_MODULE := libmmcamera_sr130pc20
+LOCAL_SRC_FILES := llibmmcamera_sr130pc20_shim.cpp
+LOCAL_MODULE := libmmcamera_sr130pc20_shim
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
