b51365c76f5f40913bebf405ffef76f7b489e4ad
matteo0026 <marmotta200026@gmail.com>
2019-03-05 20:48:41 +0100

    Remove wvm_shim Since DRM is fixed after updating the libs

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -9,14 +9,6 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := wvm_shim.cpp
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation liblog libmedia libcutils
-LOCAL_MODULE := libwvm_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libmmcamera_sr130pc20_shim.cpp
 LOCAL_MODULE := libmmcamera_sr130pc20_shim
 LOCAL_SHARED_LIBRARIES := liblog
