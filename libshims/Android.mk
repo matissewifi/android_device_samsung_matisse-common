e99c80a88d222ec816f7dd3314740db93de63009
matteo0026 <marmotta200026@gmail.com>
2019-03-05 13:03:17 +0100

    Fix libvwm_shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -6,17 +6,13 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
 LOCAL_MODULE := libshim_ril
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := wvm_shim.cpp
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshim_wvm
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation liblog libmedia libcutils
+LOCAL_MODULE := libwvm_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libmmcamera_sr130pc20_shim.cpp
 LOCAL_MODULE := libmmcamera_sr130pc20_shim
 LOCAL_SHARED_LIBRARIES := liblog
