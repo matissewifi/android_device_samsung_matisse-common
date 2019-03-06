c69c1a294395380edbded2758b4aa30e3bc23bd5
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-03-05 21:10:00 -0500

    init_sec > init_matisse

--- init/Android.mk
+++ init/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init system/core/healthd bootable/recovery
 LOCAL_CFLAGS := -Wall
 LOCAL_SRC_FILES := init_matisse.cpp
-LOCAL_MODULE := init_matisse
+LOCAL_MODULE := libinit_matisse
 
 include $(BUILD_STATIC_LIBRARY)
