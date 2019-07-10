73d5b9e429b92fe69266ca3fce4991e22e454829
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-07-10 10:14:29 -0400

    Removing lib irl_shim.c
    Removing lib irl_shim.c

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,14 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ril_shim.c
-LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
-LOCAL_MODULE := libril_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libmmcamera_sr130pc20_shim.cpp
 LOCAL_MODULE := libmmcamera_sr130pc20_shim
 LOCAL_SHARED_LIBRARIES := liblog
