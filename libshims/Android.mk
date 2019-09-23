7776238fbf645152cd9f667b4f78b8348a472631
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-09-23 16:07:55 -0400

    Removing Nouget stuff

--- libshims/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libmmcamera_sr130pc20_shim.cpp
-LOCAL_MODULE := libmmcamera_sr130pc20_shim
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
